8fdf9340ea1ba58eda9e64e2d8693e6cec728c3e
oscarsu97 <zhenhuanghuang97@gmail.com>
2019-08-28 12:06:54 +0800

    Level 8. store dateTime string variable as LocalDateTime in Event and Deadline class

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,21 +1,26 @@
-public class Deadline extends Task{
-    private String dateTime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    public Deadline(String description, String dateTime){
+public class Deadline extends Task {
+    private LocalDateTime localDateTime;
+    private DateTimeFormatter dateTimeFormat = DateTimeFormatter.ofPattern("dd/MM/YYYY HHmm");
+
+    public Deadline(String description, String dateTime) {
         super(description);
-        this.dateTime = dateTime;
-    }
+        String[] dateAndTime = dateTime.split(" ");
+        String[] date = dateAndTime[0].split("/");
+        int time = Integer.parseInt(dateAndTime[1]);
+        int dayOfMonth = Integer.parseInt(date[0]);
+        int month = Integer.parseInt(date[1]);
+        int year = Integer.parseInt(date[2]);
+        int hour = time / 100;
+        int min = time % 100;
 
-    public String getDateTime() {
-        return dateTime;
-    }
-
-    public void setDateTime(String dateTime) {
-        this.dateTime = dateTime;
+        localDateTime = LocalDateTime.of(year, month, dayOfMonth, hour, min);
     }
 
     @Override
-    public String toString(){
-        return String.format("[D][%s] %s(by: %s)", getStatusIcon(), description, dateTime);
+    public String toString() {
+        return String.format("[D][%s] %s(by: %s)", getStatusIcon(), description, localDateTime.format(dateTimeFormat));
     }
 }
