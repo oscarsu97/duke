00d15a99283b63d5934b39d9b57acf13dc234dfb
oscarsu97 <zhenhuanghuang97@gmail.com>
2019-09-04 23:16:55 +0800

    Add a GUI to to Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,27 +3,29 @@ import duke.storage.Storage;
 import duke.task.TaskList;
 import duke.ui.Ui;
 
+
+
 /**
  * Represents a bot that helps user to keep track of their task list.
  * It execute task according to the user's input command.
  */
-public class Duke {
+public class Duke  {
 
     private Storage storage;
     private TaskList taskList;
     private Ui ui;
     private Parser parser;
 
+
     /**
      * initialise duke and load data from duke.txt file
      * into a TaskList class.
-     * @param filePath file path of duke.txt file
      */
-    public Duke(String filePath) {
+    public Duke() {
         ui = new Ui();
-        storage = new Storage(filePath);
+        storage = new Storage("data/duke.txt");
         taskList = new TaskList(storage.load());
-        parser = new Parser(taskList, ui);
+        parser = new Parser(taskList, ui, storage);
     }
 
     /**
@@ -34,16 +36,26 @@ public class Duke {
      */
     public void run() {
         ui.showWelcome();
-        TaskList tasks = parser.parse();
-        storage.updateFile(tasks.getTaskList());
+//        TaskList tasks = parser.parse();
+//        storage.updateFile(tasks.getTaskList());
     }
+
+
+
+
 
     /**
      * main method where the code starts from.
+     *
      * @param args arguments
      */
     public static void main(String[] args) {
-        new Duke("data/duke.txt").run();
+        new Duke().run();
+    }
+
+    public String getResponse(String input) {
+        String response = parser.parse(input);
+        return response;
     }
 }
 
