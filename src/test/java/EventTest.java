9ebb3b02a6f8c5e8ff91868482e6f70333179ead
oscarsu97 <zhenhuanghuang97@gmail.com>
2019-09-16 23:01:06 +0800

    Fix error in EventTest class

--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -1,13 +1,16 @@
 import duke.task.Event;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EventTest {
 
     @Test
     public void testToString() {
-        Event event = new Event("read book", "12/12/2019 1300");
+        Event event = new Event("read book",
+                LocalDateTime.of(2019, 12, 12, 13, 00));
         assertEquals("12/12/2019 1300", event.getDateTime());
     }
 }
